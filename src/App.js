e46eaa43e06bdad39170f78924a6a6b3b5949ab0
Jeanine Schoessler <jeanine.mt@gmail.com>
2018-02-21 19:24:17 -0700

    lift state again to app

--- src/App.js
+++ src/App.js
@@ -6,21 +6,87 @@ import Start from './Action/Start';
 import Reset from './Action/Reset';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      thisBoard: null,
+      nextBoard: null,
+      rows: 10,
+      cols: 10,
+      initalized: false
+    }
+    this.updateAlive = this.updateAlive.bind(this);
+    this.incrementGeneration = this.incrementGeneration.bind(this);
+    this.getAliveNCount = this.getAliveNCount.bind(this);
+    this.setupBoard = this.setupBoard.bind(this);
+  }
+  setupBoard(rows,cols) {
+    let htmlBoard = [];
+    for (var i = 0; i < rows; i++) {
+      let htmlRow = [];
+      for (var h = 0; h < cols; h++) {
+        htmlRow.push(false)
+      }
+      htmlBoard.push(htmlRow)
+    }
+    console.log('hi', htmlBoard)
+
+    this.setState({
+      thisBoard: htmlBoard,
+      initalized: true,
+      rows: rows,
+      cols: cols
+    })
+  }
+  
+  componentDidMount() {
+    if (this.state.thisBoard === null) {
+      this.setupBoard(5,5);
+    }
+  }
+  
+  // Number of alive neighbors adjacent
+  getAliveNCount(x,y) {
+    let neighbors = 0;
+    if (x != 0) {
+      console.log("hey it's not on the border")
+    }
+  }
+  incrementGeneration(){
+    this.getAliveNCount(1,1)
+  }
+  updateAlive(a, b) {
+    let updateAlive = this.state.thisBoard;
+    if (updateAlive[a][b]) {
+      updateAlive[a][b] = false
+    } else {
+      updateAlive[a][b] = true;
+    }
+    console.log("setting",a,b,updateAlive[a][b]);
+    this.setState({
+      thisBoard: updateAlive 
+    })
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header"> 
-          <h1 className="App-title">Game of Life</h1>
-        </header>
-        <main>
-          <Board rows={5} cols={5}/>
-          <div className="actions">
-            <Start />
-            <Reset />
-          </div>
-        </main>
-      </div>
-    );
+    if (this.state.initalized){
+      return (
+        <div className="App">
+          <header className="App-header"> 
+            <h1 className="App-title">Game of Life</h1>
+          </header>
+          <main>
+            <Board initalized={this.state.initalized} thisBoard={this.state.thisBoard} rows={this.state.rows} cols={this.state.cols} updateAlive={this.updateAlive} />
+            <div className="actions">
+              <Start />
+              <Reset />
+              Speed slider
+            </div>
+          </main>
+        </div>
+      );
+    } else {
+      return ("Loading...")
+    }
   }
 }
 
--- src/Grid/Board.js
+++ src/Grid/Board.js
@@ -4,49 +4,14 @@ import Square from './Square';
 export default class Board extends Component {
   constructor() {
     super();
-
-
-    this.state = {
-      aliveState: null,
-      initalized: false
-    }
-    this.updateAlive = this.updateAlive.bind(this);
   }
-  componentDidMount() {
-    if (this.state.aliveState === null) {
-      let htmlBoard = [];
-      for (var i = 0; i < this.props.rows; i++) {
-        let htmlRow = [];
-        for (var h = 0; h < this.props.cols; h++) {
-          htmlRow.push(false)
-        }
-        htmlBoard.push(htmlRow)
-      }
-      this.setState({
-        aliveState: htmlBoard,
-        initalized: true
-      })
-    }
-  }
-  updateAlive(a, b) {
-    let updateAlive = this.state.aliveState;
-    if (updateAlive[a][b]) {
-      updateAlive[a][b] = false
-    } else {
-      updateAlive[a][b] = true;
-    }
-    console.log("setting",a,b,updateAlive[a][b]);
-    this.setState({
-      aliveState: updateAlive 
-    })
-  }
+  
   render() {
     let boxWidth =  (100 / this.props.cols) + "%";
     let print = [];
-    if (this.state.initalized) {
-      this.state.aliveState.forEach( (element, index) => {
+      this.props.thisBoard.forEach( (element, index) => {
         element.forEach( (e, i) => {
-          print.push(<Square boxWidth={boxWidth} updateAlive={this.updateAlive} a={index} b={i} alive={e} />)
+          print.push(<Square boxWidth={boxWidth} updateAlive={this.props.updateAlive} a={index} b={i} alive={e} />)
         });
       });
     return (
@@ -54,8 +19,5 @@ export default class Board extends Component {
         {print}
       </div>
     );
-    } else {
-      return ( "Loading..." )
-    }
   }
 }
